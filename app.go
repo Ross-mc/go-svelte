bc8afc0c9fe76b099cc335ce87985981952e8fc2
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-12-27 16:02:01 +0000

    Adding test post response and default

--- app.go
+++ app.go
@@ -10,7 +10,15 @@ import (
 const PORT = "8080"
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World\n")
+	switch r.Method {
+	case "GET":
+		io.WriteString(w, "Hello World\n")
+	case "POST":
+		io.WriteString(w, "Post request reponse\n")
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
 }
 
 func main() {
